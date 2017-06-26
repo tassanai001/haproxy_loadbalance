d5f84928afb15428d039eda61c87996c1e23b224
Tassanai Yeeton <tassanai.yeeton@gmail.com>
2017-06-26 14:21:14 +0700

    Add Balanceing and Fixable Modules

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-docker rmi -f haproxylab
-docker build -t haproxylab .
+if [ $1 = "balanceing" ]
+ then
+    echo "============= balanceing ============="
+    docker rmi -f haproxylab
+    docker build -f Dockerfile.balanceing -t haproxylab .
+elif [ $1 = "fixable" ]
+ then
+    echo "============= fixable ============="
+    docker rmi -f haproxylab
+    docker build -f Dockerfile.fixable -t haproxylab .
+else
+    echo "============ POOLNODE RUN ================="
+    echo "Please Enter Parameters [ balanceing , fixable ]"
+fi
